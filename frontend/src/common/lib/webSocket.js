9f57dfb8039e42c5b94d8a4dd4402eb8a0996507
Ji2z <leegin98@naver.com>
2021-08-05 16:15:19 +0900

    refactor : 참가자 비디오 목록 수정(S05P13A306-68)

--- frontend/src/common/lib/webSocket.js
+++ frontend/src/common/lib/webSocket.js
@@ -3,7 +3,11 @@ var ws = new WebSocket('wss://' + "localhost:8443" + '/groupcall');
 function sendMessage(message) {
 	var jsonMessage = JSON.stringify(message);
 	console.log('Sending message: ' + jsonMessage);
-	ws.onopen = () => ws.send(jsonMessage);
+	console.log('state : ',ws.readyState)
+	if (ws.readyState == 0)
+		ws.onopen = () => ws.send(jsonMessage);
+	else
+		ws.send(jsonMessage);
 }
 
 export {sendMessage, ws};
