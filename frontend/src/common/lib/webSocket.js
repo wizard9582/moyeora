3fc038a9033d0caaeec76f3502855128c9918f58
최성훈 <skyworking@naver.com>
2021-08-09 15:33:06 +0900

    feature/api-board(게시판 관련 API 구현) develop에 병합

--- frontend/src/common/lib/webSocket.js
+++ frontend/src/common/lib/webSocket.js
@@ -1,4 +1,4 @@
-var ws = new WebSocket('wss://' + "localhost:8443" + '/groupcall');
+var ws = new WebSocket('wss://' + "52.79.219.86"+":8443" + '/groupcall');
 
 function sendMessage(message) {
 	var jsonMessage = JSON.stringify(message);
