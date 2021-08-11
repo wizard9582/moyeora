b04272891a054ed2b00a1ce6620894d45e3da62c
Ji2z <leegin98@naver.com>
2021-08-11 23:14:48 +0900

    refactor : 어노테이션 수정(S05P13A306-10)

--- backend/src/main/java/com/ssafy/db/repository/ConferenceHistoryRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/ConferenceHistoryRepository.java
@@ -2,6 +2,8 @@ package com.ssafy.db.repository;
 
 import com.ssafy.db.entity.ConferenceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConferenceHistoryRepository extends JpaRepository<ConferenceHistory, Long> {
 }
--- backend/src/main/java/com/ssafy/db/repository/UserConferenceRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/UserConferenceRepository.java
@@ -2,6 +2,8 @@ package com.ssafy.db.repository;
 
 import com.ssafy.db.entity.UserConference;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserConferenceRepository extends JpaRepository<UserConference, Long> {
 }
