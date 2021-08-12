8224e6f50230f96ede7ea61ef00c60545845d4f8
노진혁 <qkfskan82@naver.com>
2021-08-12 18:21:57 +0900

    feat : 링크복사 기능 수정

--- frontend/src/components/game/pop/invite-pop.vue
+++ frontend/src/components/game/pop/invite-pop.vue
@@ -2,15 +2,15 @@
   <el-dialog title="친구 초대하기" v-model="state.popupVisible" @close="handleClose">
     <el-form :label-position="state.align">
       <el-form-item prop="friend" label="초대할친구" :label-width="state.formLabelWidth">
-        <el-input></el-input>
+        <el-input v-model="state.friendName" @change="changeName"></el-input>
       </el-form-item>
       <el-form-item prop="link" label="초대 링크" :label-width="state.formLabelWidth">
-        <textarea id="invite-link">https://localhost:8080/game/123/</textarea>
-        <el-button type="primary" @click="copyLink">링크 복사</el-button>
+          <el-input id="invite-link" type="textarea" :rows="2" v-model="state.inviteLink"></el-input>
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="footer">
+        <el-button type="primary" @click="copyLink">링크 복사</el-button>
         <el-button type="primary" @click="handleClose">닫기</el-button>
       </span>
     </template>
@@ -19,6 +19,7 @@
 
 <script>
 import { reactive, computed } from 'vue'
+import { useRouter } from 'vue-router';
 
 export default {
   name: "InvitePop",
@@ -31,13 +32,15 @@ export default {
   },
 
   setup(props, { emit }) {
+    const router = useRouter()
+
     const state = reactive({
       formLabelWidth: '120px',
-      inviteLink: '',
+      inviteLink: window.location.href,
       align: 'left',
       popupVisible: computed(() => props.open),
+      friendName: "",
     })
-
     // 링크를 클립보드에 복사해주는 함수
     // 우선은 textarea로 설정 input text 로도 가능
     const copyLink = () => {
@@ -47,11 +50,18 @@ export default {
       document.execCommand("copy")
     }
 
+    const changeName = () =>{
+      let name = state.friendName
+      //axios로 회원 list 받아오기
+
+    }
+
     const handleClose = () => {
+      state.friendName=""
       emit('closeInvitePopup')
     }
 
-    return { state, copyLink, handleClose }
+    return { state, copyLink, handleClose, changeName }
   }
 }
 </script>
